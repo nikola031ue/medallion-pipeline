f7a3e4ed7901accdc5559db7dee9744fa7b272b9
Nikola Kostic <49210841+nikola031ue@users.noreply.github.com>
2026-05-29 13:09:55 +0200

    feat: Lambda normalization for hacker news data #6

--- cdk/app.py
+++ cdk/app.py
@@ -3,6 +3,7 @@ import aws_cdk as cdk
 from stacks.network_stack import NetworkStack
 from stacks.data_lake_stack import DataLakeStack
 from stacks.bronze_stack import BronzeStack
+from stacks.silver_stack import SilverStack
 
 app = cdk.App()
 
@@ -16,6 +17,15 @@ data_lake = DataLakeStack(app, "DataLakeStack", env=env)
 BronzeStack(
     app,
     "BronzeStack",
+    vpc=network.vpc,
+    lambda_sg=network.lambda_sg,
+    bucket=data_lake.bucket,
+    env=env,
+)
+
+SilverStack(
+    app,
+    "SilverStack",
     vpc=network.vpc,
     lambda_sg=network.lambda_sg,
     bucket=data_lake.bucket,
